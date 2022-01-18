c45ba33707c37a010e6acaf640b6f1fa1241a4f5
root <root@ip-172-31-87-112.ec2.internal>
2022-01-18 14:40:13 +0000

    Sync:e_Note~Jhn5:41 saved.. repodesc:undefined

--- account/myoj/e_Note_json.js
+++ account/myoj/e_Note_json.js
@@ -28338,7 +28338,7 @@ var _bnotes=
             "38": "",
             "39": "",
             "40": "",
-            "41": "<ol><li>man-made glory vs the glory of God. </li></ol>",
+            "41": "<ol><li>man-made glory vs the glory of God. </li><li>the glory of God belong to God. the glory of man belong to man. </li></ol>",
             "42": "",
             "43": "",
             "44": "",
