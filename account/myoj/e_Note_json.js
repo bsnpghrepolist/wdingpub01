170d0e8d2422a5174ce98fb7f99c90429cf2a6f9
root <root@ip-172-31-87-112.ec2.internal>
2022-03-05 21:23:34 +0000

    Sync:e_Note~Jhn17:17 saved.. repodesc:undefined

--- account/myoj/e_Note_json.js
+++ account/myoj/e_Note_json.js
@@ -28886,7 +28886,7 @@ var _bnotes=
             "14": "",
             "15": "<ol><li>Is culture neutral or good or bad?</li><li>Is language diversity good or bad? </li><li>Culture and world (cusmos) relationship. What is culture?</li><li>What is evil(panerus)?</li><li>What’s the evil in the world?</li></ol><div><br></div>",
             "16": "<ol><li>The world = culture. </li></ol>",
-            "17": "",
+            "17": "<ol><li>Sanctify us from peneros(evil) in world. </li></ol>",
             "18": "",
             "19": "",
             "20": "<ol><li>Disciples and their followers and Jesus relationship.  </li></ol>",
