5c69f660bec5a2e41cdb4c7a8d09b3b01840ccab
root <root@ip-172-31-87-112.ec2.internal>
2022-10-13 23:57:32 +0000

    Sync:len:468,dlt:458. repodesc:undefined

--- account/dat/MostRecentSearches_json.js
+++ account/dat/MostRecentSearches_json.js
@@ -20,5 +20,16 @@ var MostRecentSearches = {
             "glory": "221013 163650,results",
             "a": "221013 163650,results"
         }
+    ],
+    "Chrome": [
+        {
+            "life": "221013 180635,19, NIV, UserDef: Mal Mat",
+            "Jesus": "221013 175441,209 found in 'NIV' in 'Mal Mat'.",
+            "粪土": "221013 174620,1 found in 'NT' of 'CUVS'.",
+            "live by": "221013 172910,3 found in 'NIV' for 'Heb Jas 1Pe 2Pe 1Jn 2Jn 3Jn Jud.'",
+            "God": "221013 163650,results",
+            "glory": "221013 163650,results",
+            "a": "221013 163650,results"
+        }
     ]
 }
