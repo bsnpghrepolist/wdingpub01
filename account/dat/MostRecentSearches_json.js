09f88bfa8646dc62a9dc5be3dbb1dd365579522c
root <root@ip-172-31-87-112.ec2.internal>
2022-10-14 03:00:14 +0000

    Sync:len:866,dlt:376. repodesc:undefined

--- account/dat/MostRecentSearches_json.js
+++ account/dat/MostRecentSearches_json.js
@@ -1,6 +1,12 @@
 var MostRecentSearches = {
     "MostRecentSearch": [
         {
+            "[\"live by\",15,\"ESV\",\"all\"]": "221014 025638",
+            "[\"live by\",29,\"NIV\",\"all\"]": "221014 025407",
+            "[\"life\",526,\"ESV\",\"all\"]": "221014 022258",
+            "[\"life\",18,\"NIV\",\"Matthew\"]": "221014 000233",
+            "[\"glory\",3,\"NIV\",\"Matthew\"]": "221014 000050",
+            "[\"life\",575,\"NIV\",\"all\"]": "221013 235845",
             "life": "221013 180635,19, NIV, UserDef: Mal Mat",
             "Jesus": "221013 175441,209 found in 'NIV' in 'Mal Mat'.",
             "粪土": "221013 174620,1 found in 'NT' of 'CUVS'.",
