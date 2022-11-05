84ebc98e5bb1a545662621dfb1691d26b5fb5396
root <root@ip-172-31-87-112.ec2.internal>
2022-11-05 06:17:29 +0000

    Sync:len:undefined,dlt:1176. repodesc:undefined

--- account/dat/MostRecentVerses_json.js
+++ account/dat/MostRecentVerses_json.js
@@ -188,5 +188,27 @@ var MostRecentVerses = {
             "2Co5:21": "221013 092252",
             "Gal5:16": "221013 092059"
         }
-    ]
+    ],
+    "MostRecent_Searches": {
+        "MostRecentSearch": [
+            {
+                "[\"live by\",29,\"NIV\",\"all\"]": "221104 183630",
+                "[\"live by\",29,\"NIV\",\"\"]": "221103 230631",
+                "[\"live by\",0,\"NIV\",\"Amos\"]": "221103 230621",
+                "[\"live by\",10,\"NIV\",\"Paulines\"]": "221014 110403",
+                "[\"live by\",15,\"ESV\",\"all\"]": "221014 025638",
+                "[\"life\",526,\"ESV\",\"all\"]": "221014 022258",
+                "[\"life\",18,\"NIV\",\"Matthew\"]": "221014 000233",
+                "[\"glory\",3,\"NIV\",\"Matthew\"]": "221014 000050",
+                "[\"life\",575,\"NIV\",\"all\"]": "221013 235845",
+                "life": "221013 180635,19, NIV, UserDef: Mal Mat",
+                "Jesus": "221013 175441,209 found in 'NIV' in 'Mal Mat'.",
+                "粪土": "221013 174620,1 found in 'NT' of 'CUVS'.",
+                "live by": "221013 172910,3 found in 'NIV' for 'Heb Jas 1Pe 2Pe 1Jn 2Jn 3Jn Jud.'",
+                "God": "221013 163650,results",
+                "glory": "221013 163650,results",
+                "a": "221013 163650,results"
+            }
+        ]
+    }
 }
