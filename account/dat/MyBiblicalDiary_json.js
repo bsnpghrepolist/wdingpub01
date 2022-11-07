ea1700bda953d72624ba5baa207fbd28e265150b
root <root@ip-172-31-87-112.ec2.internal>
2022-11-07 20:13:37 +0000

    Sync:len:undefined. repodesc:undefined

--- account/dat/MyBiblicalDiary_json.js
+++ account/dat/MyBiblicalDiary_json.js
@@ -1,5 +1,5 @@
 var MyBiblicalDiary = {
     "2022": {
-        "1107": "<span style=\"-webkit-text-size-adjust: auto; font-family: Times; font-variant-ligatures: normal; orphans: 2; widows: 2; text-decoration-thickness: initial;\">Jesus did not recommend us to take part in politics. But He did not curse politicians, Through politics, we can know the world, and through it, we can know the sign of the world, the will of God, to testify what Jesus ever said to us, the power of Jesus.-- no matter what your career is, as long as you work the kingdom of God, you are the disciple of him.&nbsp;</span><br>"
+        "1107": "<span style=\"-webkit-text-size-adjust: auto; font-family: Times; font-variant-ligatures: normal; orphans: 2; widows: 2; text-decoration-thickness: initial;\">Jesus did not recommend us to take part in politics. But He did not imply politicians are evil. <br>Through politics, we can know the world, and through it, we can know the sign of the world, the will of God, to testify what Jesus ever said to us, the power of Jesus.-- no matter what your career is, as long as you work the kingdom of God, you are the disciple of him.&nbsp;</span><br>"
     }
 }
