26d070e7784a3956a3f9bdf594c8828d35a7956c
root <root@ip-172-31-87-112.ec2.internal>
2022-11-16 03:28:09 +0000

    Sync:ok. repodesc:undefined

--- account/dat/MyBiblicalDiary_json.js
+++ account/dat/MyBiblicalDiary_json.js
@@ -14,7 +14,7 @@ var MyBiblicalDiary = {
         "1114": "-Jesus is the Holy Spirit.&nbsp;<ol><li>Jesus miracle prove he is a spirit.&nbsp;</li><li>Jesus words prove he is a spirit.&nbsp;</li><li>The Word became the flesh.&nbsp;</li></ol>",
         "1115": "<ol><li>Faith=&gt;Honest heart =&gt;naive=&gt; Simple. </li><li>Police is completely complicated. Pre tended to a good man, pretended to be a bad man. The real contains fake, the fake contains real. Repeated unlimited. But God know everything.&nbsp;</li></ol>",
         "1116": "<br>",
-        "1122": "Pc off",
+        "1122": "<br>",
         "1129": "<br>",
         "1130": "WD lead&nbsp;",
         "0105": "as"
