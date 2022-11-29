1e26771f0beb71f0d26fcf704a6ebb46941b4ed3
Ubuntu <ubuntu@ip-172-31-87-112.ec2.internal>
2022-11-29 20:48:04 +0000

    Updates

---
+++ dat/MyBiblicalDiary_json.js
@@ -0,0 +1,28 @@
+var MyBiblicalDiary = {
+    "2021": {
+        "0113": "trt",
+        "0106": "uu<ol><li>ui</li><li>oop</li></ol>",
+        "0105": "addd",
+        "0103": "new year eve"
+    },
+    "2022": {
+        "1107": "<span style=\"-webkit-text-size-adjust: auto; font-family: Times; font-variant-ligatures: normal; orphans: 2; widows: 2; text-decoration-thickness: initial;\">Jesus did not recommend us to take part in politics. But He did not imply politicians are evil. <br>Politics is like money. It can be used for both good and evil.<br>Through politics, we can better understand the world God created. Politics is a layer of heaven above money.&nbsp; God created many heavens.&nbsp;<br>Christians need to know politics but should not be addicted to it.&nbsp;<br>Through understand the politics, we can know the sign of the world, the will of God, to testify what Jesus ever said to us, the power of Jesus.-- no matter what your career is, as long as you work for the kingdom of God, you are the disciple of him.&nbsp;<br></span><br><br>How do Christians live under a dictatorship ruler without freedom of speech?&nbsp;<br>(1) political resistance&nbsp;<br>(2) hun (abide)<br>(3) run&nbsp;<br>(4) dsd",
+        "1108": "Anger makes things worse. Let Jesus do it.&nbsp;",
+        "1109": "-don’t be against enemies. But against to myself. Attach self body. &nbsp;Let my body abide me.&nbsp;<br>Love your enemies,&nbsp;<br><br><br>Chinese language is a ambiguous language. Interpretation can be many but contradictory each other.&nbsp;<br><br>But the elements of composition are identical to biblical.&nbsp;<br><br>Eg. People took food for heaven.&nbsp;",
+        "1110": "-don’t be against enemies&nbsp;<div>-but love your enemies. How?</div><div><br></div><div>Jx: then no enemies in the world.&nbsp;</div><div>Wd: there is only one struggle—believe him or not.&nbsp;</div>",
+        "1113": "- Why does Jewish race keep low in population among world? &nbsp;It’s a form of the role in leadership.&nbsp;<br><br>- those who were reborn imply that they get out of the world. But firstly they must get of of their culture where they were born.&nbsp;<br><br>Some of the cultures are easier than the other to get rid of.&nbsp;<br><br>The difference languages are created by God.&nbsp;<br>It’s role is to help man under stand the plural of heaven.&nbsp;<br><br><br>",
+        "1114": "-Jesus is the Holy Spirit.&nbsp;<ol><li>Jesus miracle prove he is a spirit.&nbsp;</li><li>Jesus words prove he is a spirit.&nbsp;</li><li>The Word became the flesh.&nbsp;</li></ol>",
+        "1115": "<ol><li>Faith=&gt;Honest heart =&gt;naive=&gt; Simple. </li><li>Politics is completely complicated. Pre tended to a good man, pretended to be a bad man. The real contains fake, the fake contains real. Repeated unlimited. But God know everything. </li><li>Christians must accept a fact that the world become worse and the kingdom of God become greater. </li><li>The western democracy system will be destroyed by the east dragon.  But the kingdom of Heaven will reach to the end of the earth. </li><li>Nerve connects to God. Do not Let our nerve connect to the world.&nbsp;</li></ol>",
+        "1116": "Dennis lead <ol><li>Greg: the separated family children. Pray for them. </li><li>ding: mom. Patience.&nbsp;</li><li>Jerry: to Lexington. Her Roommate plan the baptism. Help church event.&nbsp;</li><li>Dennis: being content. Consultant. Phil Lan. Video conference &nbsp;</li></ol>",
+        "1117": "Uo00p",
+        "1122": "<br>",
+        "1123": "Pc off",
+        "1127": "The story of the Jesus is the story above all stories.&nbsp;",
+        "1129": "<br>",
+        "1130": "WD lead&nbsp;",
+        "0105": "as"
+    },
+    "2023": {
+        "0104": "w<ol><li>ew</li></ol>"
+    }
+}
