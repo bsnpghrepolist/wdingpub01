6869d515375409668d1f833ca85c4c75934fb04d
Ubuntu <ubuntu@ip-172-31-80-239.ec2.internal>
2022-12-11 01:22:30 +0000

    Updates

---
+++ account/dat/MostRecentSearches_json.js
@@ -0,0 +1,45 @@
+var MostRecentSearches = {
+    "MostRecentSearch": [
+        {
+            "[\"live by\",10,\"NIV\",\"Paulines\"]": "221014 110403",
+            "[\"live by\",15,\"ESV\",\"all\"]": "221014 025638",
+            "[\"live by\",29,\"NIV\",\"all\"]": "221014 025407",
+            "[\"life\",526,\"ESV\",\"all\"]": "221014 022258",
+            "[\"life\",18,\"NIV\",\"Matthew\"]": "221014 000233",
+            "[\"glory\",3,\"NIV\",\"Matthew\"]": "221014 000050",
+            "[\"life\",575,\"NIV\",\"all\"]": "221013 235845",
+            "life": "221013 180635,19, NIV, UserDef: Mal Mat",
+            "Jesus": "221013 175441,209 found in 'NIV' in 'Mal Mat'.",
+            "粪土": "221013 174620,1 found in 'NT' of 'CUVS'.",
+            "live by": "221013 172910,3 found in 'NIV' for 'Heb Jas 1Pe 2Pe 1Jn 2Jn 3Jn Jud.'",
+            "God": "221013 163650,results",
+            "glory": "221013 163650,results",
+            "a": "221013 163650,results"
+        }
+    ],
+    "Temp": [
+        {
+            "life": "221013 180635,19, NIV, UserDef: Mal Mat",
+            "Jesus": "221013 175441,209 found in 'NIV' in 'Mal Mat'.",
+            "粪土": "221013 174620,1 found in 'NT' of 'CUVS'.",
+            "live by": "221013 172910,3 found in 'NIV' for 'Heb Jas 1Pe 2Pe 1Jn 2Jn 3Jn Jud.'",
+            "God": "221013 163650,results",
+            "glory": "221013 163650,results",
+            "a": "221013 163650,results"
+        }
+    ],
+    "Chrome": [
+        {
+            "[\"life\",18,\"NIV\",\"Matthew\"]": "221014 000233",
+            "[\"glory\",3,\"NIV\",\"Matthew\"]": "221014 000050",
+            "[\"life\",575,\"NIV\",\"all\"]": "221013 235845",
+            "life": "221013 180635,19, NIV, UserDef: Mal Mat",
+            "Jesus": "221013 175441,209 found in 'NIV' in 'Mal Mat'.",
+            "粪土": "221013 174620,1 found in 'NT' of 'CUVS'.",
+            "live by": "221013 172910,3 found in 'NIV' for 'Heb Jas 1Pe 2Pe 1Jn 2Jn 3Jn Jud.'",
+            "God": "221013 163650,results",
+            "glory": "221013 163650,results",
+            "a": "221013 163650,results"
+        }
+    ]
+}
