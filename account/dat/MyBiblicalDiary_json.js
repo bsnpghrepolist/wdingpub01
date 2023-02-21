9cbf6c862bcb795cd0959373a689005cb40a7b66
root <root@ip-172-31-80-239.ec2.internal>
2023-02-21 06:33:00 +0000

    do git_add_commit_push_Sync(true).

--- account/dat/MyBiblicalDiary_json.js
+++ account/dat/MyBiblicalDiary_json.js
@@ -38,6 +38,7 @@ var MyBiblicalDiary_json =
         "0223": "wei i min testi",
         "0216": "Jerry testimony. defer.<div>Dennis: Nelson. Adams Drews, in church.</div><div>Jerry: trip&nbsp;</div><div>WD: pray for the safe trip, and wisdom of preaching, control the amygadala.&nbsp;</div><div>Greg: Susan Sunday memorial service. greving process.&nbsp;</div><div>comfort Ross. 10,12 sons.&nbsp;<br><div><br><div><br><div><br></div></div></div></div>",
         "0309": "1 min testimony.<div><br></div>",
-        "0220": "BEijing arrived.&nbsp;"
+        "0220": "BEijing arrived.&nbsp;",
+        "0221": "XieHe hospital&nbsp;"
     }
 }
