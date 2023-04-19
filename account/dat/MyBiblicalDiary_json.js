2e55b516450edbb3dbf1f798fe9d36e935d0869a
Ubuntu <ubuntu@ip-172-31-80-239.ec2.internal>
2023-04-19 23:16:08 +0000

    Updates

--- account/dat/MyBiblicalDiary_json.js
+++ account/dat/MyBiblicalDiary_json.js
@@ -61,6 +61,6 @@ var MyBiblicalDiary_json =
         "0413": "p293, 2. <div>jx medicine taken lzxdjn</div>",
         "0510": "王国伟 中医科",
         "0419": "Jx eye",
-        "0420": "p307"
+        "0420": "p307.<div>last unit of class.&nbsp;</div><div>The power of association.</div>"
     }
 }
