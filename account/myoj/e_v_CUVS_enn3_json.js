c8dc14837c1338f2ff61e3b81a5d8d6d9abeffdf
root <root@ip-172-31-87-112.ec2.internal>
2023-06-03 22:08:34 +0000

    do git_add_commit_push_Sync(true).

--- account/myoj/e_v_CUVS_enn3_json.js
+++ account/myoj/e_v_CUVS_enn3_json.js
@@ -9985,7 +9985,7 @@ var CUVsen=
             "3": "Ahab 将他的家宰 Pbadiah 召了来。 Obadiah 甚是敬畏 HYWY 。",
             "4": "Jezebel 杀 HYWY 众先知的时候、 Obadiah 将一百个先知藏了、每五十人藏在一个洞里、拿饼和水供养他们。",
             "5": "Ahab 对 Obadiah 说、我们走遍这地、到一切水泉旁、和一切溪边、或者找得着青草、可以救活骡马、免得绝了牲畜。",
-            "6": "于是二人分地游行、 Ahab 独走一路、 Pbadiah 独走一路。",
+            "6": "于是二人分地游行、 Ahab 独走一路、 Obadiah 独走一路。",
             "7": " Pbadiah 在路上恰与 Eliah-or-elijah 相遇、 Pbadiah 认出他来、就俯伏在地、说、你是我主 Eliah-or-elijah 不是。",
             "8": "回答说、是、你去告诉你主人、说、 Eliah-or-elijah 在这里。",
             "9": " Pbadiah 说、仆人有甚么罪、你竟要将我交在 Ahab 手里、使他杀我呢。",
