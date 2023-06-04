b852a6f107c490f74fce3c083cc7c0cd7cfd4e42
root <root@ip-172-31-87-112.ec2.internal>
2023-06-04 12:55:45 +0000

    do git_add_commit_push_Sync(true).

--- account/myoj/e_v_CUVS_enn3_json.js
+++ account/myoj/e_v_CUVS_enn3_json.js
@@ -10415,7 +10415,7 @@ var CUVsen=
             "21": "Johoram 率领所有的战车、往Zair去．夜间起来、攻打围困他的 Edom 人和车兵长． Judas 兵就逃跑、各回各家去了。",
             "22": "这样、 Edom 人背叛 Judas 、脱离他的权下、直到今日。那时 Libnah 人也背叛了。",
             "23": " Johoram 其余的事、凡他所行的都写在 Judas 列王记上。",
-            "24": " Johoram 与他列祖同睡、葬在 David 城、他列祖的坟地里．他儿子 Ahoaziah 接续他作王。",
+            "24": "Johoram 与他列祖同睡、葬在 David 城、他列祖的坟地里．他儿子 Ahaziah 接续他作王。",
             "25": " Israel 王 Ahab 的儿子 Johoram 十二年、 Judas 王 Johoram 的儿子 Ahab 谢登基。",
             "26": "他登基的时候、年二十二岁、在 Jerusalem 作王一年．他母亲名叫 Athaliah 、是 Israel 王 Omri 的孙女。",
             "27": " Ahab 谢效法 Ahab 家行 HYWY 眼中看为恶的事、与 Ahab 家一样．因为他是 Ahab 家的女婿。",
