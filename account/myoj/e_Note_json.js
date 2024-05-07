0c64c4c2a818740fcdf95fb2b5345394a941778d
root <root@ip-172-31-87-112.ec2.internal>
2024-05-07 14:12:00 +0000

    do git_add_commit_push_Sync(true).

--- account/myoj/e_Note_json.js
+++ account/myoj/e_Note_json.js
@@ -32447,7 +32447,7 @@ var _bnotes=
             "10": "240507_091247(ed).<ol><li>Serving Saint=serving pastor=for showing in the world.</li><li><a href=\"https://chs.goldenlampstand.org/lib/epistles/read.php?b=Heb&p=2.2.4\">https://chs.goldenlampstand.org/lib/epistles/read.php?b=Heb&p=2.2.4</a></li><li><br></li></ol>",
             "11": "240507_092107(ed).<ol><li>show to God first, not show to people, not show in media or public.</li><li>show people only if it builds the kingdom of heaven.</li></ol>",
             "12": "240507_092529(ed).<ol><li>Rom5:3-5 about the chain.</li><li>Mat13:23 good soil parable.</li></ol>",
-            "13": "",
+            "13": "240507_101152(ed).<ol><li>the love of God. </li></ol>",
             "14": "",
             "15": "",
             "16": "",
