36964c79c9a831c72a7e925d4f4c8230a540ddf5
root <root@ip-172-31-87-112.ec2.internal>
2024-05-31 13:26:43 +0000

    do git_add_commit_push_Sync(true).

--- account/myoj/e_Note_json.js
+++ account/myoj/e_Note_json.js
@@ -7632,7 +7632,7 @@ var _bnotes=
             "22": ""
         },
         "2": {
-            "1": "240531_084423(ed).<ol><li>How wealthy? A stewardship. </li><li>恶霸=大财主 in Chinese culture. </li></ol>",
+            "1": "240531_092632(ed).<ol><li>How wealthy? A stewardship. </li><li>恶霸=大财主 in Chinese culture. </li><li>Mat1:5. </li></ol>",
             "2": "240531_084610(ed).<ol><li>Character of Ruth: </li><li>Immigrant, language, faith differ. </li><li>Native culture. Rich, friendly. But worship gods. </li><li>Soft meek among people</li><li>Lonely</li><li>Humble </li><li>Strong faith love the Lord the truth. </li><li>USA: Jesus based decling. </li><li>Human relationship is love. How?  Pray bless others enemy. </li></ol>",
             "3": "240531_085025(ed).<ol><li>Deu24:19</li></ol>",
             "4": "240531_082241(ed).<ol><li>A blessing man. =Love. </li></ol>",
