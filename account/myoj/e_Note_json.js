9e8f69f23e871fe8137f44de501781fd224f6888
root <root@ip-172-31-87-112.ec2.internal>
2024-08-07 13:55:05 +0000

    do git_add_commit_push_Sync(true).

--- account/myoj/e_Note_json.js
+++ account/myoj/e_Note_json.js
@@ -8233,7 +8233,7 @@ var _bnotes=
             "30": "240806_083804(ed).<ol><li>Chinese translation issue. </li><li>David, fine. </li></ol>"
         },
         "19": {
-            "1": "240807_095313(ed).<ol><li>Jonathan: son of Saul, successor of the King.</li><li>Loyal to the load or bio father.</li><li>Make them a good relationship.</li></ol>",
+            "1": "240807_095503(ed).<ol><li>Jonathan: son of Saul, successor of the King.</li><li>Loyal to the load or bio father.</li><li>Make them a good relationship. \"Blessed are the peacemakers, for they will be called sons of God.\" (Mat5:9 NIV)</li></ol>",
             "2": "",
             "3": "",
             "4": "",
