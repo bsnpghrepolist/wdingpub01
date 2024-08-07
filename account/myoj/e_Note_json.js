2de90df8a80198c300760d18ca133daf851dcc11
root <root@ip-172-31-87-112.ec2.internal>
2024-08-07 13:50:56 +0000

    do git_add_commit_push_Sync(true).

--- account/myoj/e_Note_json.js
+++ account/myoj/e_Note_json.js
@@ -8233,7 +8233,7 @@ var _bnotes=
             "30": "240806_083804(ed).<ol><li>Chinese translation issue. </li><li>David, fine. </li></ol>"
         },
         "19": {
-            "1": "",
+            "1": "240807_095054(ed).<ol><li>Jonathan: son of Saul, successor of the King</li></ol>",
             "2": "",
             "3": "",
             "4": "",
