1040ee6498aae3269e1d0e3de87c337f563b3f2d
root <root@ip-172-31-87-112.ec2.internal>
2024-12-27 14:50:19 +0000

    do git_add_commit_push_Sync(true).

--- account/myoj/e_Note_json.js
+++ account/myoj/e_Note_json.js
@@ -9766,7 +9766,7 @@ var _bnotes=
             "31": "",
             "32": "",
             "33": "",
-            "34": "241227_095000(ed).<ol><li>Moses cannot enter Cannan; David cannot build temple; </li></ol>",
+            "34": "241227_095016(ed).<ol><li>Moses cannot enter Canaan; David cannot build temple; </li></ol>",
             "35": "",
             "36": "",
             "37": "",
